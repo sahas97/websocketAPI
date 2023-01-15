b98fbf6deeea0c6a6db237dea3916f8715bdb58d
sahas97 <65613126+sahas97@users.noreply.github.com>
2023-01-15 16:37:53 +0530

    updated

--- sever.js
+++ sever.js
@@ -3,18 +3,6 @@ const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 const port = process.env.PORT || 3000;
-const https = require('https');
-
-app.post('/serverip', (req, res) => {
-    https.get('https://api.render.com/network/ip', (response) => {
-        response.on('data', (data) => {
-            let serverIp = JSON.parse(data).ip;
-            res.json({ip: serverIp});
-        });
-    }).on('error', (err) => {
-        res.json({error: err.message});
-    });
-});
 
 io.on('connection', (socket) => {
     console.log('A user connected');
